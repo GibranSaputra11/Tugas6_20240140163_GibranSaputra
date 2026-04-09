39bcdfea1ad6f9b322c9ed55dea3ecdfaa438a4d
Gibran Saputra <gibranbantul@gmail.com>
2026-04-09 21:33:53 +0700

    Tugas 6 Gibran

---
+++ jsCH5.js
@@ -0,0 +1,9 @@
+video.addEventListener("click", function video() {
+    alert("Anda mengklik video")
+});
+audio.addEventListener("click", function audio() {
+    alert("Anda mengklik audio")
+});
+gambar.addEventListener("click", function gambar() {
+    alert("Anda mengklik gambar")
+});
